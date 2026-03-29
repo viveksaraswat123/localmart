6e7f6b2509e8d9d3f0795b660adc89602b4ca532
viveksaraswat49 <viveksaraswat49@gmail.com>
2026-03-29 19:35:50 +0530

    removed unnecessary files

--- main.py
+++ main.py
@@ -571,8 +571,3 @@ def delete_order(
     db.delete(order)
     db.commit()
     return {"message": "Order deleted"}
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
